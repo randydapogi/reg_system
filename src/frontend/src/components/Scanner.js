1d2fc74cba2c2032f06689f2950e753d0db8eeae
Randy Beros <randyberos@MacBook-Pro.local>
2019-07-31 11:45:51 +0800

    asd

--- src/frontend/src/components/Scanner.js
+++ src/frontend/src/components/Scanner.js
@@ -64,9 +64,29 @@ class Scanner extends Component {
     }
 
     onTakePhoto(data){
+        var byteString;
+        var dataURI = this.webcam.getScreenshot();
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        var new_img =  new Blob([ia], {type:mimeString});
+
+
+
         this.setState({
             // image: data,
-            image: this.webcam.getScreenshot(),
+            image: new_img,
             hasImage: true
         })
     }
